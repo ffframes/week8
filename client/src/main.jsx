460585f04a1531cf0b6e18d3436d3fb82152ed69
Faisal Rehman <faisalcm07@gmail.com>
2026-02-24 14:29:04 +0000

    main.jsx update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,24 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { ChakraProvider, createSystem, defineConfig } from "@chakra-ui/react"
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
 import { BrowserRouter } from 'react-router'
 
 
-const config = defineConfig({
-  theme: {
-    tokens: {
-      colors: {},
-    },
-  },
-})
 
-const system = createSystem(config)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    
-    <ChakraProvider value={system}>
+    <ChakraProvider value={defaultSystem}>
       <BrowserRouter><App /></BrowserRouter>
     </ChakraProvider>
     
